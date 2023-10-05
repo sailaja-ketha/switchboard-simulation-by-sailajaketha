5b11564d678aad86bc1113afb16f33d97c1780c1
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-05 10:55:59 +0530

    changes

--- Program.cs
+++ Program.cs
@@ -5,37 +5,39 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("**********Switch Board Simulation************");
-            Switchboard switchboard = new Switchboard();
-
             Console.Write("Enter the number of fans: ");
-            int noOfFans = int.Parse(Console.ReadLine());
-            switchboard.AddAppliance("Fan", noOfFans);
+            int numOfFans = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter the number of ACs: ");
-            int noOfACs = int.Parse(Console.ReadLine());
-            switchboard.AddAppliance("AC", noOfACs);
+            int numOfACs = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter the number of bulbs: ");
-            int noOfBulbs = int.Parse(Console.ReadLine());
-            switchboard.AddAppliance("Bulb", noOfBulbs);
+            int numOfBulbs = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
+            Switchboard switchboard = new Switchboard();
+
+
+            for (int i = 1; i <= numOfFans; i++)
             {
-                switchboard.ShowSwitchBoardMenu();
-                Console.WriteLine("Enter the appliance number to control or to back:");
-                int choice = int.Parse(Console.ReadLine());
-                if (choice >= 1)
-                {
-                    Console.WriteLine("1. Switch On");
-                    Console.WriteLine("2. Back");
-                    int option = int.Parse(Console.ReadLine());
-
-                    if (option == 1)
-                    {
-                        switchboard.ChangeApplianceState(choice-1);
-                    }
-                }
+                Appliance fan = new Appliance($"Fan {i}");
+                Switch fanSwitch = new Switch(fan);
+                switchboard.AddSwitch(fanSwitch);
             }
+
+            for (int i = 1; i <= numOfACs; i++)
+            {
+                Appliance ac = new Appliance($"AC {i}");
+                Switch acSwitch = new Switch(ac);
+                switchboard.AddSwitch(acSwitch);
+            }
+            for (int i = 1; i <= numOfBulbs; i++)
+            {
+                Appliance bulb = new Appliance($"Bulb {i}");
+                Switch bulbSwitch = new Switch(bulb);
+                switchboard.AddSwitch(bulbSwitch);
+            }
+
+            switchboard.Run();
         }
     }
 }
